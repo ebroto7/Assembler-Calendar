63b989efb31f1e645858ddcb41c3181f8cd313f0
Kayn Mertens <adaygarcia336@gmail.com>
2023-09-04 09:44:00 +0200

    import desde calendar.ts

---
+++ src/Views/Calendar.ts
@@ -0,0 +1,23 @@
+export function getDate(dateKey:number) {
+    const yearOffset = (dateKey - 32) % 512;
+    const year = (dateKey - 32 - yearOffset) / 512;
+    const day = yearOffset % 32;
+    const month = (yearOffset - day) / 32;
+    return new Date(year + 1970, month, day);
+  }
+
+export function getToday(){
+    const date = new Date;
+    return date;
+}
+export function getMonth(){
+    const date = new Date;
+    return date.getMonth()+1;
+}
+
+export function getDay(){
+    const day = getToday().getDate();
+    return day;
+}
+
+
