eac9ead6bc33b109e9d922234d7dcb5c9f9bbed7
Ian Pares <yannpares@gmail.com>
2023-09-04 09:37:38 +0200

    new branch and inital functions JS

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// import { getDate, printDate } from "./Views/Calendar.js";
+
+
+function getDate(dateKey:number) {
+    const yearOffset = (dateKey - 32) % 512;
+    const year = (dateKey - 32 - yearOffset) / 512;
+    const day = yearOffset % 32;
+    const month = (yearOffset - day) / 32;
+    return new Date(year + 1970, month, day);
+  }
+
+function getToday(){
+    const date = new Date;
+    return date;
+}
+function getMonth(){
+    const date = new Date;
+    return date.getMonth()+1;
+}
+
+function getDay(){
+    const day = getToday().getDate();
+    return day;
+}
+
+console.log(getToday());
+console.log(getMonth());
+console.log(getDay());
+
+
+console.log(Date.now());
+
+console.log(getDate(Date.now()));
