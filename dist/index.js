933419229e996613aded035d15010951f0e218f7
Kayn Mertens <adaygarcia336@gmail.com>
2023-09-04 10:26:53 +0200

    hice un tsc

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,8 @@
-function getDate(dateKey) {
-    const yearOffset = (dateKey - 32) % 512;
-    const year = (dateKey - 32 - yearOffset) / 512;
-    const day = yearOffset % 32;
-    const month = (yearOffset - day) / 32;
-    return new Date(year + 1970, month, day);
-}
-function getToday() {
-    const date = new Date;
-    return date;
-}
-function getMonth() {
-    const date = new Date;
-    return date.getMonth() + 1;
-}
-function getDay() {
-    const day = getToday().getDate();
-    return day;
-}
-console.log(getToday());
-console.log(getMonth());
-console.log(getDay());
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Calendar_js_1 = require("./Views/Calendar.js");
+console.log((0, Calendar_js_1.getToday)());
+console.log((0, Calendar_js_1.getMonth)());
+console.log((0, Calendar_js_1.getDay)());
 console.log(Date.now());
-console.log(getDate(Date.now()));
+console.log((0, Calendar_js_1.getDate)(Date.now()));
