0a73a999d206a4c1c4d140700530b4a4861ed566
Amina Seye <aminaseye2@icloud.com>
2023-09-07 09:12:30 +0200

    Checkbox done

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,27 @@
 "use strict";
+const asideBar = document.querySelector("#aside");
+const calendarContainer = document.querySelector("#calendarContainer");
+const selectCalendarContainer = document.querySelector("#selectCalendarContainer");
+selectCalendarContainer.style.backgroundColor = "green";
+const createArray = ["trabajo", "gimnasio", "cumple"];
+function createCheckboxCalendar() {
+    const checkboxCalendar = document.createElement("div");
+    asideBar.appendChild(checkboxCalendar);
+    createArray.forEach((element) => {
+        const InputyLabel = document.createElement("div");
+        const createInput = document.createElement("input");
+        createInput.setAttribute("class", "form-check-input");
+        createInput.setAttribute("type", "checkbox");
+        createInput.setAttribute("value", "");
+        createInput.setAttribute("id", "flexCheckDefault");
+        const createLabel = document.createElement("label");
+        createLabel.setAttribute("class", "form-check-label");
+        createLabel.setAttribute("name", "my calendars");
+        createLabel.setAttribute("for", "flexCheckDefault");
+        createLabel.innerText = element;
+        InputyLabel.appendChild(createInput);
+        InputyLabel.appendChild(createLabel);
+        checkboxCalendar.appendChild(InputyLabel);
+    });
+}
+createCheckboxCalendar();
