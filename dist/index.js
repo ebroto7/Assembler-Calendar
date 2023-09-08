ddb4cc230fc32882746d483948b94b23c1c71d55
Amina Seye <aminaseye2@icloud.com>
2023-09-08 13:08:52 +0200

    (Add) Calendar to fix

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,66 @@
 "use strict";
 const asideBar = document.querySelector("#aside");
 const createArray = ["trabajo", "gimnasio", "cumple"];
+const actual = new Date();
+function showCalendar(year, month) {
+    const now = new Date(year, month - 1, 1);
+    const last = new Date(year, month, 0);
+    const firstDayWeek = (now.getDay() == 0) ? 7 : now.getDay();
+    const lastdayMonth = last.getDate();
+    var day = 0;
+    var result = "<tr bgcolor='silver'>";
+    const today = 0;
+    const last_cell = firstDayWeek + lastdayMonth;
+    for (let i = 1; i <= 42; i++) {
+        if (i = firstDayWeek) {
+            day = 1;
+        }
+        if (i < firstDayWeek || i >= last_cell) {
+            result += "<td>&nbsp;</td>";
+        }
+        else {
+            if (day == actual.getDate() && month == actual.getMonth() + 1 &&
+                year == actual.getFullYear()) {
+                result += "<td class='hoy'>" + day + "</td>";
+            }
+            else {
+                result += "<td>" + day + "</td>";
+                day++;
+            }
+            if (i % 7 == 0) {
+                if (day > lastdayMonth)
+                    break;
+                result += "</tr><tr>\n";
+            }
+        }
+        result += "</tr>";
+    }
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var nextMonth = month + 1;
+    var nextYear = year;
+    if (month + 1 > 12) {
+        nextMonth = 1;
+        nextYear = year + 1;
+    }
+    var prevMonth = month - 1;
+    var prevYear = year;
+    if (month - 1 < 1) {
+        prevMonth = 12;
+        prevYear = year - 1;
+    }
+    const calendarTable = document.getElementById("calendarTable");
+    calendarTable.innerHTML = "<div>" + months[month - 1] + " / " + year + "</div>";
+    const prevMY = document.getElementById("prevMY");
+    const nextMY = document.getElementById("nextMY");
+    prevMY === null || prevMY === void 0 ? void 0 : prevMY.addEventListener("click", () => {
+        prevMY.innerHTML = "prevYear", "prevMonth";
+    });
+    nextMY === null || nextMY === void 0 ? void 0 : nextMY.addEventListener("click", () => {
+        nextMY.innerHTML = "prevMonth", "nextMonth";
+    });
+    calendarTable.innerHTML = result;
+}
+showCalendar(actual.getFullYear(), actual.getMonth() + 1);
 const checkboxCalendar = document.createElement("div");
 function createCheckboxCalendar() {
     asideBar.appendChild(checkboxCalendar);
